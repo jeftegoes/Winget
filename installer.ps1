3cf68c63e7140e8cc3ac2ef637dff1558255a28b
Jefté Goes <jefte.goes.dev@outlook.com>
2023-06-30 22:47:00 -0700

    Update script.

--- installer.ps1
+++ installer.ps1
@@ -1,14 +1,41 @@
-$apps = @(
-    "OpenJS.NodeJS.LTS"
+$apps_silent = @(
+    "OpenJS.NodeJS.LTS",
+    "Notepad++.Notepad++",
+    "WinMerge.WinMerge",
+    "Mozilla.Firefox",
+    "Valve.Steam",
+    "VideoLAN.VLC",
+    "Git.Git",
+    "Microsoft.SQLServerManagementStudio",
+    "7zip.7zip",
+    "Python.Python.3.11",
+    "9NKSQGP7F2NH" # WhatsApp
 )
+
+$gui = @(
+    "Microsoft.VisualStudioCode"
+)
+
+function silent_installation {
+    foreach ($currentApp in $apps_silent) {
+        Write-Host -ForegroundColor Yellow "Installing:" $currentApp
+        winget install --silent --exact --scope machine $currentApp
+        Write-Host -ForegroundColor Green "Installed:" $currentApp
+    }
+}
+
+function gui_installation {
+    foreach ($currentApp in $gui) {
+        Write-Host -ForegroundColor Yellow "Installing:" $currentApp
+        winget install --interactive --exact --scope machine $currentApp
+        Write-Host -ForegroundColor Green "Installed:" $currentApp
+    }
+}
 
 Clear-Host
 Write-Warning "Starting installation..."
 
-foreach ($currentApp in $apps) {
-    Write-Host -ForegroundColor Yellow "Installing:" $currentApp
-    winget install --silent --exact --scope machine $currentApp
-    Write-Host -ForegroundColor Green "Installed:" $currentApp
-}
+silent_installation
+gui_installation
 
 Write-Warning "Finished installation..."
